817ac59b9baf2ee3dbb356a3ccffc85c5c8f5a3c
Boyd007 <105176171+Boyd007@users.noreply.github.com>
2022-06-23 17:03:41 -0400

    InitialCommit

---
+++ Quiz.js
@@ -0,0 +1,43 @@
+var timeLeft = 30
+function countdown() {
+    var timeInterval = setInterval(function () {
+ timeLeft--;
+        console.log(timeLeft)
+        var timer = document.querySelector("#timer-countdown");
+        timer.textContent = timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timeInterval); 
+        
+        alert("Time's Up")
+        var enterscore = prompt("Enter Your Initials")
+        document.getElementById("user-name-1").textContent=enterscore
+        }
+}, 1000);
+} 
+countdown()
+var buttonAnswer = document.querySelectorAll('.answer');
+buttonAnswer.forEach(button => {
+   button.addEventListener("click", function(event) {
+console.log(event.target.classlist)
+if(event.target.classList[1]){
+event.target.classList.add("green")
+if (event.target.getAttribute("last-question")){
+alert("Game Is Over")
+var enterscore = prompt("Enter Your Initials")
+}
+} else{
+    if (event.target.getAttribute("last-question")){
+        alert("Game Is Over")
+        var enterscore = prompt("Enter Your Initials")
+        }
+    event.target.classList.add("red")
+    timeLeft-=5
+
+}
+
+
+
+
+
+});  
+});
